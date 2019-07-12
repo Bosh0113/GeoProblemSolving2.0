f8b14e62c6e83d32366d903fa60e666e83c92c9a
mzy <bryate_mzy@163.com>
2019-07-12 14:59:16 +0800

    文件夹结构更新

--- GeoProblemSolving-front/src/router/index.js
+++ GeoProblemSolving-front/src/router/index.js
@@ -7,27 +7,29 @@ Vue.use(Router)
 
 
 const routes = [{
-  path: '/', name: 'Navigation',component: resolve=>(require(["@/components/Navigation"],resolve)), children: [
+  path: '/', name: 'Navigation',component: resolve=>(require(["@/components/navigation"],resolve)), children: [
     { path: '', redirect: 'home'},
-    { path: 'home', name: 'Home', component: resolve=>(require(["@/components/NavigationContent/Home"],resolve)) },
-    { path: 'projectlist', name: 'Projects', component: resolve=>(require(["@/components/Projects/ProjectList"],resolve)) },
-    { path: 'project/:id', name: 'ProjectDetail', component: resolve=>(require(["@/components/NavigationContent/ProjectDetail"],resolve)) },
-    { path: 'project/:id/workspace', name: 'workspace', component: resolve=>(require(["@/components/NavigationContent/moduleList"],resolve)) },
-    { path: 'project/:id/subproject', name: 'subproject', component: resolve=>(require(["@/components/SubProject/SubprojectDetail"],resolve)) },
-    { path: 'newproject', name: 'NewProject', component: resolve=>(require(["@/components/createNew/NewProject"],resolve)) },
-    { path: 'participants', name: 'Participants', component: resolve=>(require(["@/components/NavigationContent/Participants"],resolve)) },
-    { path: 'community', name: 'Community', component: resolve=>(require(["@/components/Community/Community"],resolve)) },
-    { path: 'community/:id', name: 'Communityreply', component: resolve=>(require(["@/components/Community/CommunityReply"],resolve)) },
-    { path: 'help', name: 'Help', component: resolve=>(require(["@/components/NavigationContent/Help"],resolve)) },
-    { path: 'personalPage', name: 'PersonalPage', component: resolve=>(require(["@/components/UsersState/PersonalPage"],resolve)) },
-    { path: 'notifications', name: 'Notifications', component: resolve=>(require(["@/components/UsersState/Notifications"],resolve)) },
-    { path: 'memberPage/:id', name: 'MemberDetailPage', component: resolve=>(require(["@/components/UserPage/memberDetailPage"],resolve)) },
-    { path: 'resourceList', name: 'resourceList', component: resolve=>(require(["@/components/Resource/ResourceList"],resolve)) },
-    { path: 'publicResource', name: 'PublicResource', component: resolve=>(require(["@/components/Resource/PublicResourceList"],resolve)) },
+    { path: 'home', name: 'Home', component: resolve=>(require(["@/components/navigationContent/home"],resolve)) },
+    { path: 'projectlist', name: 'Projects', component: resolve=>(require(["@/components/navigationContent/projectList"],resolve)) },
+    { path: 'project/:id', name: 'ProjectDetail', component: resolve=>(require(["@/components/projects/projectDetail"],resolve)) },
+    { path: 'project/:id/workspace', name: 'workspace', component: resolve=>(require(["@/components/workingSpace/moduleList"],resolve)) },
+    { path: 'project/:id/subproject', name: 'subproject', component: resolve=>(require(["@/components/subProject/subprojectDetail"],resolve)) },
+    { path: 'newproject', name: 'NewProject', component: resolve=>(require(["@/components/projects/newProject"],resolve)) },
+    { path: 'participants', name: 'Participants', component: resolve=>(require(["@/components/navigationContent/participants"],resolve)) },
+    { path: 'community', name: 'Community', component: resolve=>(require(["@/components/community/community"],resolve)) },
+    { path: 'community/:id', name: 'Communityreply', component: resolve=>(require(["@/components/community/communityReply"],resolve)) },
+    { path: 'help', name: 'Help', component: resolve=>(require(["@/components/navigationContent/help"],resolve)) },
+    { path: 'personalPage', name: 'PersonalPage', component: resolve=>(require(["@/components/userPage/personalPage"],resolve)) },
+    { path: 'notifications', name: 'Notifications', component: resolve=>(require(["@/components/usersState/notifications"],resolve)) },
+    { path: 'memberPage/:id', name: 'MemberDetailPage', component: resolve=>(require(["@/components/userPage/memberDetailPage"],resolve)) },
+    { path: 'resourceList', name: 'resourceList', component: resolve=>(require(["@/components/resource/resourceList"],resolve)) },
+    { path: 'publicResource', name: 'PublicResource', component: resolve=>(require(["@/components/resource/publicResourceList"],resolve)) },
+    { path: 'join/:id/:email', name: 'joinProject', component: resolve=>(require(["@/components/Projects/joinNewProject"],resolve)) },
+    { path: 'login', name: 'Login', component: resolve=>(require(["@/components/usersState/login"],resolve)) },
+    { path: 'register', name: 'Register', component: resolve=>(require(["@/components/usersState/register"],resolve)) },
+    { path: 'resetPassword/:email', name: 'resetPassword', component: resolve=>(require(["@/components/UsersState/resetPwd"],resolve)) } 
   ]
 },
-{ path: '/login', name: 'Login', component: resolve=>(require(["@/components/UsersState/Login"],resolve)) },
-{ path: '/register', name: 'Register', component: resolve=>(require(["@/components/UsersState/Register"],resolve)) },
 { path: '/chat', name: 'chatUtil', component: resolve=>(require(["@/components/utils/chatroom"],resolve)) },
 { path: '/draw', name: 'drawUtil', component: resolve=>(require(["@/components/utils/drawBoard"],resolve)) },
 { path: '/map', name: 'mapTool', component: resolve=>(require(["@/components/utils/mapTool"],resolve)) },
@@ -37,8 +39,6 @@ const routes = [{
 { path: '/nc/charts', name: 'dataChartsNC', component: resolve=>(require(["@/components/utils/singleUtils/ncCharts"],resolve)) },
 { path: '/video', name: 'videoViewer', component: resolve=>(require(["@/components/utils/videoViewer"],resolve)) },
 { path: '/preview', name: 'pdfViewer', component: resolve=>(require(["@/components/utils/filePreview"],resolve)) },
-{ path: '/join/:id/:email', name: 'joinProject', component: resolve=>(require(["@/components/createNew/joinNewProject"],resolve)) },
-{ path: '/resetPassword/:email', name: 'resetPassword', component: resolve=>(require(["@/components/UsersState/ResetPwd"],resolve)) },
 { path: '/tinymce', name: 'tinymce', component: resolve=>(require(["@/components/utils/singleUtils/tinymce"],resolve)) },
 
 ]
