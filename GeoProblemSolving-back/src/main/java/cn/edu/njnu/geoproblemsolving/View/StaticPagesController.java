6c1ad61c1ebe6b34e5e3a17db14710fabdfda828
orangemimi <mimi@users.noreply.github.com>
2019-07-15 19:33:45 +0800

    Update StaticPagesController.java

--- GeoProblemSolving-back/src/main/java/cn/edu/njnu/geoproblemsolving/View/StaticPagesController.java
+++ GeoProblemSolving-back/src/main/java/cn/edu/njnu/geoproblemsolving/View/StaticPagesController.java
@@ -5,10 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
-@Controller
+@RestController
 @RequestMapping(value = "/static")
 public class StaticPagesController {
     @Resource
