cea52529a9d22d5f0f686ad0ccde495f8b952078
BeiChen Zhang <45487945+Bosh0113@users.noreply.github.com>
2019-08-01 11:39:37 +0800

    大小写

---
+++ GeoProblemSolving-front/src/components/navigation.vue
@@ -0,0 +1,370 @@
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#logo {
+  position: absolute;
+  width: 129px;
+  height: 40px;
+  z-index: 1;
+  margin-top: 5px;
+  margin-left: 2.5%;
+  cursor: pointer;
+}
+.header span {
+  font-size: 15px;
+}
+.container {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+header {
+  height: 60px;
+  top: 0;
+  z-index: 100;
+  position: absolute;
+  width: 100%;
+  flex: 0 0 auto;
+  min-width: 1200px;
+}
+.content {
+  flex: 1 0 auto;
+  margin-top: 60px;
+}
+footer {
+  background-color: #515a6e;
+  height: 60px;
+  width: 100%;
+  bottom: 0;
+  flex: 0 0 auto;
+}
+.userState {
+  position: absolute;
+  margin-left: 85%;
+  width: 15%;
+  top: 0px;
+  z-index: 1;
+}
+.navPart {
+  width: 85%;
+}
+.menuItem span {
+  font-size: 1.2em;
+  font-weight: bold;
+}
+.menuItem:first-child {
+  /* margin-left: 25%; */
+}
+.userImg {
+  width: 40px;
+  height: 40px;
+  vertical-align: middle;
+}
+.footerTop {
+  text-align: center;
+  color: white;
+  font-weight: bold;
+  margin-top: 10px;
+}
+.footerBottom {
+  text-align: center;
+  color: white;
+  font-size: 0.8em;
+}
+</style>
+<template>
+  <div class="container">
+    <header :style="absoluteStyle">
+      <img
+        src="@/assets/images/OGMS.png"
+        id="logo"
+        class="pic"
+        @click="goHome"
+        style="cursor:pointer;margin-left:5%"
+      >
+      <div class="navPart">
+        <Menu
+          mode="horizontal"
+          theme="dark"
+          active-name="home"
+          @on-select="turnContent"
+          :style="`z-index:0;background:`+headerBgColor"
+          width="auto"
+        >
+          <MenuItem name="home" class="menuItem" style="margin-left:35%">
+            <span>Home</span>
+          </MenuItem>
+          <MenuItem name="projects" class="menuItem">
+            <span>Projects</span>
+          </MenuItem>
+          <MenuItem name="resources" class="menuItem">
+            <span>Resources</span>
+          </MenuItem>
+          <!-- <MenuItem name="community" class="menuItem">
+            <span>Community</span>
+          </MenuItem> -->
+          <MenuItem name="help" class="menuItem">
+            <span>Help</span>
+          </MenuItem>
+        </Menu>
+        <div class="userState">
+          <Menu
+            mode="horizontal"
+            theme="dark"
+            @on-select="unlogin"
+            :style="`z-index:0;background:`+headerBgColor"
+            v-show="!userState"
+            class="menuItem"
+          >
+            <MenuItem name="login">
+              <span>Login</span>
+            </MenuItem>
+            <MenuItem name="register">
+              <span>Sign up</span>
+            </MenuItem>
+          </Menu>
+          <Menu
+            mode="horizontal"
+            theme="dark"
+            @on-select="logged"
+            :style="`z-index:0;background:`+headerBgColor"
+            v-show="userState"
+            class="menuItem"
+          >
+            <MenuItem name="notification">
+              <Badge :count="unreadNoticeCount" id="noticeBadge">
+                <Icon type="ios-notifications-outline" size="25"></Icon>
+              </Badge>
+            </MenuItem>
+            <MenuItem name="personal" style="width:100px">
+              <Dropdown @on-click="changeSelect" placement="bottom-start">
+                <div @click="toPersonalPage">
+                  <img
+                    v-bind:src="avatar"
+                    v-if="avatar!=''&&avatar!=undefined&&avatar!=null"
+                    :title="userName"
+                    style="width:40px;height:40px;vertical-align:middle;"
+                  >
+                  <avatar
+                    :username="userName"
+                    :size="40"
+                    style="margin-top:10px"
+                    :title="userName"
+                    v-else
+                  ></avatar>
+                </div>
+                <DropdownMenu slot="list">
+                  <!-- <DropdownItem name="personalPage">User Space</DropdownItem> -->
+                  <DropdownItem name="logout">Log out</DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+            </MenuItem>
+          </Menu>
+        </div>
+      </div>
+    </header>
+    <section class="content" :style="{minHeight:contentHeight}">
+      <router-view @sendNotice="sendMessage" @readNotification="readNotification"></router-view>
+    </section>
+    <footer>
+      <h2 class="footerTop">
+        <i>Open Geographic Modeling and Simulation</i>
+      </h2>
+      <p class="footerBottom">Copyright © 2013-2019 OpenGMS. All rights reserved.</p>
+    </footer>
+  </div>
+</template>
+<script>
+import Avatar from "vue-avatar";
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      //消息机制
+      noticeSocket: null,
+      unreadNoticeCount: 0,
+      timer: null,
+      //导航栏宽度
+      headerWidth: "",
+      contentHeight: window.innerHeight - 120 + "px"
+    };
+  },
+  mounted() {
+    if(window.innerHeight > 675){
+      this.contentHeight = window.innerHeight - 120 + "px";
+    }
+    else{
+      this.contentHeight = 675 - 120 + "px";
+    }
+    this.headerWidth = window.innerWidth + "px";
+    if (this.$store.getters.userState) {
+      this.setTimer();
+      this.initWebSocket();
+      this.getUnreadNoticeCount();
+    }
+    window.addEventListener("resize", this.reSize);
+  },
+  beforeDestroy: function() {
+    window.removeEventListener("resize", this.reSize);
+  },
+  updated() {
+    $(".userState sup").css("margin-top", "20px");
+  },
+  components: {
+    Avatar
+  },
+  computed: {
+    userState() {
+      return this.$store.getters.userState;
+    },
+    userName() {
+      return this.$store.getters.userName;
+    },
+    avatar() {
+      return this.$store.getters.avatar;
+    },
+    headerBgColor() {
+      return this.$route.name === "Home" ? "none" : "";
+    },
+    absoluteStyle() {
+      return this.$route.name === "Home"
+        ? "position:absolute"
+        : "position:fixed";
+    }
+  },
+  methods: {
+    reSize() {
+      if (window.innerHeight > 675) {
+        this.contentHeight = window.innerHeight - 120 + "px";
+      } else {
+        this.contentHeight = 675;
+      }
+    },
+    turnContent(name) {
+      if (name === "home") {
+        this.$router.replace({ name: "Home" });
+      } else if (name == "projects") {
+        this.$router.replace({ name: "Projects" });
+      } else if (name == "resources") {
+        this.$router.replace({ name: "PublicResource" });
+      } else if (name == "community") {
+        this.$router.replace({ name: "Community" });
+      } else if (name == "help") {
+        this.$router.replace({ name: "Help" });
+      }
+    },
+    goHome() {
+      this.$router.push({ name: "Home" });
+    },
+    unlogin(name) {
+      if (name === "login") {
+        this.$router.push({ name: "Login" });
+      } else if (name == "register") {
+        this.$router.push({ name: "Register" });
+      }
+    },
+    logged(name) {
+      if (name === "notification") {
+        this.$router.push({ name: "Notifications" });
+      } else if (name === "personal") {
+      }
+    },
+    toPersonalPage() {
+      this.$router.push({ name: "PersonalPage" });
+    },
+    // 获取到通知的数量
+    getUnreadNoticeCount() {
+      this.unreadNoticeCount = 0;
+      //get请求发送的是用户id
+      this.axios
+        .get(
+          "/GeoProblemSolving/notice/inquiry" +
+            "?key=recipientId" +
+            "&value=" +
+            this.$store.getters.userId
+        )
+        .then(res => {
+          let noticeList = res.data;
+          let unreadCount = 0;
+          for (let i = 0; i < noticeList.length; i++) {
+            if (noticeList[i].state === "unread") {
+              unreadCount++;
+              continue;
+            }
+          }
+          this.$set(this, "unreadNoticeCount", unreadCount);
+        })
+        .catch(err => {
+          console.log("失败的原因是" + err.data);
+        });
+    },
+    initWebSocket() {
+      if (this.noticeSocket != null) {
+        this.noticeSocket = null;
+      }
+      var noticeSocketURL = "ws://localhost:8081/GeoProblemSolving/NoticeSocket";
+      // var noticeSocketURL = "ws://"+this.$store.state.IP_Port+"/GeoProblemSolving/NoticeSocket";
+      this.noticeSocket = new WebSocket(noticeSocketURL);
+      this.noticeSocket.onopen = this.onOpen;
+      this.noticeSocket.onmessage = this.onMessage;
+      this.noticeSocket.onclose = this.onClose;
+      this.noticeSocket.onerror = this.onError;
+    },
+    onOpen() {
+      // console.log("NoticeSocket连接成功！");
+    },
+    onMessage(e) {
+      if (e.data == "Notice") {
+        let newCount = this.unreadNoticeCount + 1;
+        this.$set(this, "unreadNoticeCount", newCount);
+        this.$Message.info("You have a new notice!");
+      } else {
+        console.log(e.data);
+      }
+    },
+    onClose(e) {
+      this.removeTimer();
+      // console.log("NoticeSocket连接断开！");
+    },
+    onError(e) {
+      this.removeTimer();
+      // console.log("NoticeSocket连接错误！");
+    },
+    sendMessage(recipientId) {
+      this.noticeSocket.send(recipientId);
+    },
+    setTimer() {
+      var that = this;
+      this.timer = setInterval(() => {
+        if (that.noticeSocket != null && that.noticeSocket != undefined) {
+          that.noticeSocket.send("ping");
+        }
+      }, 20000);
+    },
+    removeTimer() {
+      clearInterval(this.timer);
+    },
+    readNotification() {
+      let newCount = this.unreadNoticeCount;
+      if (newCount > 0) {
+        this.unreadNoticeCount = newCount - 1;
+      }
+    },
+    changeSelect(name) {
+      if (name == "logout") {
+        this.axios
+          .get("/GeoProblemSolving/user/logout")
+          .then(res => {
+            this.$store.commit("userLogout");
+            if(this.noticeSocket != null && this.noticeSocket != undefined){
+              this.noticeSocket.close();
+            }
+            this.$router.replace({ name: "Home" });
+          })
+          .catch(err => {
+            confirm("logout fail!");
+          });
+      }
+    }
+  }
+};
+</script>
---
+++ GeoProblemSolving-front/src/components/usersState/login.vue
@@ -0,0 +1,349 @@
+<style scoped>
+/* 注意css在哪里生效,需要全局使用就全局定义，仅在某个局部定义就在某个div或者元素下定义，在其子元素生效 */
+.layout {
+  --layout-background: lightgray;
+  --headerHeight: 60px;
+  --footerHeight: 60px;
+  --loginDivWidth:40%;
+  --loginTextAlign:center;
+  overflow: hidden;
+}
+.header {
+  /* 计算属性，可以根据需求动态变化宽高 */
+  height: calc(var(--headerHeight) * 1);
+  background: #515a6e;
+}
+
+.content {
+  background-color: var(--layout-background);
+  display: flex;
+  justify-content: center;
+}
+.loginDiv {
+  width: var(--loginDivWidth);
+  border: var(--loginBorder);
+  --loginContentColor: white;
+  --loginFontColor: white;
+  --loginTitleColor: #57a3f3;
+  --loginBtnFontSize: 15px;
+}
+.loginTitle {
+  background-color: var(--loginTitleColor);
+  text-align: var(--loginTextAlign);
+  vertical-align: middle;
+  line-height: 60px;
+  color: var(--loginFontColor);
+  font-size: 30px;
+  font-weight: bold;
+  padding: 0;
+}
+.login-content {
+  background-color: var(--loginContentColor);
+  padding: 50px 20px 20px 20px;
+}
+#logo {
+  position: absolute;
+  width: 129px;
+  height: 40px;
+  z-index: 1;
+  margin-top: 5px;
+  margin-left: 2.5%;
+}
+.loginBtn {
+  font-size: var(--loginBtnFontSize);
+}
+.loginBtn:hover {
+  color: white;
+  background-color: var(--loginTitleColor);
+  font-size: var(--loginBtnFontSize);
+}
+img {
+  cursor: pointer;
+}
+.reUseDiv {
+  display: flex;
+  text-align: var(--loginTextAlign);
+  justify-content: center;
+}
+.reciverSpan {
+  width: 100px;
+  text-align: var(--loginTextAlign);
+}
+.resetReuseDiv {
+  display: flex;
+  align-items: center;
+  padding: 10px;
+}
+.resetReuseDiv p,
+.resetReuseDiv input {
+  width: 100%;
+}
+</style>
+<template>
+  <div class="layout">
+    <!-- <div class="header">
+      <img src="@/assets/images/OGMS.png" id="logo" @click="goHome">
+    </div> -->
+    <div class="content" ref="homePage" v-if="UserState===false" v-bind:style="contentStyle">
+      <div class="loginDiv" v-bind:style="loginStyle">
+        <div class="loginTitle">Log in</div>
+        <div class="login-content">
+          <Form ref="loginForm" :model="loginForm" :rules="loginFormRule">
+            <FormItem prop="user" label="E-mail" :label-width="100">
+              <Input
+                placeholder="Please input your username"
+                style="width: 90%"
+                v-model="loginForm.user"
+                type="text"
+              />
+            </FormItem>
+            <FormItem prop="password" label="Password" :label-width="100">
+              <Input
+                placeholder="Please input your password"
+                style="width: 90%"
+                v-model="loginForm.password"
+                @keyup.enter.native="login('loginForm')"
+                type="password"
+              />
+            </FormItem>
+            <div class="reUseDiv">
+              <Checkbox v-model="checked">Automatic login within one week</Checkbox>
+            </div>
+            <div class="reUseDiv">
+              <p>
+                Forget password?
+                <a @click="resetModalSHow=true">Reset</a>
+              </p>
+            </div>
+            <br>
+            <FormItem>
+              <div class="reUseDiv">
+                <Button
+                  type="default"
+                  @click="login('loginForm')"
+                  class="loginBtn"
+                >Log in</Button>
+                <Button
+                  type="default"
+                  @click="register"
+                  class="loginBtn"
+                  style="margin-left:10px"
+                >Sign up</Button>
+              </div>
+            </FormItem>
+          </Form>
+        </div>
+      </div>
+    </div>
+    <Modal
+      v-model="resetModalSHow"
+      @on-ok="sendResetEmail"
+      @on-cancel
+      ok-text="Confirm"
+      cancel-text="Cancel"
+      title="Reset password board"
+    >
+      <div class="resetReuseDiv">
+        <span class="reciverSpan">reciever:</span>
+        <Input v-model="loginForm.user"/>
+      </div>
+      <div class="resetReuseDiv">
+        <Icon type="ios-information-circle-outline" :size="20" color="lightblue"/>
+        <p>We will send you an email with a url you can visit it and reset your password, if you agree, you can click the Confirm button and you will get an email soon.</p>
+      </div>
+    </Modal>
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  computed: {
+    UserState() {
+      return this.$store.getters.userState;
+    },
+  },
+  created(){
+    if(this.$store.getters.userState){
+      this.$router.replace({ name: "PersonalPage" });
+    }
+  },
+  data() {
+    return {
+      loginForm: {
+        user: "",
+        password: ""
+      },
+      loginFormRule: {
+        user: [
+          {
+            required: true,
+            message: "User name can not be empty",
+            trigger: "blur"
+          }
+        ],
+        password: [
+          {
+            required: true,
+            message: "Password can not be empty",
+            trigger: "blur"
+          },
+          {
+            type: "string",
+            min: 6,
+            message: "The length of password can not be less than 6 characters",
+            trigger: "blur"
+          }
+        ]
+      },
+      loginStyle: {
+        marginTop: ""
+      },
+      contentStyle: {
+        height: ""
+      },
+      checked: false,
+      changePwdEmailStyle:
+        "This email is used for help you reset your password, you can click this URL ",
+      urlAddress: 'http://'+this.$store.state.IP_Port+"/GeoProblemSolving/resetPassword/",
+      resetModalSHow: false
+    };
+  },
+  mounted() {
+    // this.$Notice.config({
+    //   top: 100,
+    //   duration: 0
+    // });
+    this.contentStyle.height = window.innerHeight - 60 + "px";
+    this.loginStyle.marginTop = window.innerHeight / 5 + "px";
+    this.getlocalStorage();
+  },
+  methods: {
+    login(form) {
+      this.$refs[form].validate(valid => {
+        if (valid) {
+          if (this.checked == true) {
+            localStorage.setItem("user", this.loginForm.user);
+            var password = this.loginForm.password;
+            password = this.encrypto(password);
+            localStorage.setItem("password", password);
+            localStorage.setItem("statusRecord", this.checked);
+          } else if (this.checked == false) {
+            localStorage.setItem("user", "");
+            localStorage.setItem("password", "");
+            localStorage.setItem("statusRecord", false);
+          }
+          var email = this.loginForm.user;
+          var passwordFro = this.loginForm.password;
+          var passwordAES = this.encrypto(passwordFro);
+          var passwordAESURI = window.encodeURIComponent(passwordAES);
+          this.axios
+            .get(
+              "/GeoProblemSolving/user/login" +
+                "?email=" +
+                email +
+                "&password=" +
+                passwordAESURI
+            )
+            .then(res => {
+              if (res.data === "Email") {
+                this.$Message.error("Email does not exist.");
+              } else if (res.data === "Password" || res.data === "Fail") {
+                this.$Message.error("Invalid account or password.");
+              } else {
+                this.$store.commit("userLogin", res.data);
+                //这个地方到底应该怎么跳转？？
+                this.$router.go(-1);
+                // this.$router.push({ name: "Home" });
+              }
+            });
+        } else {
+          this.$Message.error(
+            "If you don't have an account, you can regisetr one."
+          );
+        }
+      });
+    },
+    goBack() {
+      this.$router.push({ name: "Home" });
+    },
+    register() {
+      this.$router.push({ name: "Register" });
+    },
+    goHome() {
+      this.$router.push({ name: "Home" });
+    },
+    getlocalStorage() {
+      this.loginForm.user = localStorage.getItem("user");
+      this.loginForm.password = this.decrypto(localStorage.getItem("password"));
+      // 将字符串格式的true转换为boolean模式的true
+      if (localStorage.getItem("statusRecord")) {
+        this.checked = eval(localStorage.getItem("statusRecord"));
+      }
+    },
+    encrypto(context) {
+      var CryptoJS = require("crypto-js");
+      var key = CryptoJS.enc.Utf8.parse("NjnuOgmsNjnuOgms");
+      var iv = CryptoJS.enc.Utf8.parse("NjnuOgmsNjnuOgms");
+      var encrypted = "";
+      if (typeof context == "string") {
+      } else if (typeof context == "object") {
+        context = JSON.stringify(context);
+      }
+      var srcs = CryptoJS.enc.Utf8.parse(context);
+      encrypted = CryptoJS.AES.encrypt(srcs, key, {
+        iv: iv,
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.Pkcs7
+      });
+      return encrypted.toString();
+    },
+    decrypto(context) {
+      var CryptoJS = require("crypto-js");
+      var key = CryptoJS.enc.Utf8.parse("NjnuOgmsNjnuOgms");
+      var iv = CryptoJS.enc.Utf8.parse("NjnuOgmsNjnuOgms");
+      var decrypt = CryptoJS.AES.decrypt(context, key, {
+        iv: iv,
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.Pkcs7
+      });
+      var decryptedStr = decrypt.toString(CryptoJS.enc.Utf8);
+      return decryptedStr.toString();
+    },
+    goRegister() {
+      this.$router.push({ name: "Register" });
+    },
+    sendResetEmail() {
+      var emailFormBody = {};
+      emailFormBody["recipient"] = this.loginForm.user;
+      emailFormBody["mailTitle"] = "Reset password notification";
+      emailFormBody["mailContent"] =
+        this.changePwdEmailStyle +
+        this.urlAddress +
+        this.loginForm.user +
+        " to change your password, thanks.";
+      this.axios
+        .post("/GeoProblemSolving/email/send", emailFormBody)
+        .then(res => {
+          if (res.data == "Success") {
+            this.$Notice.success({
+              title: "Email send success",
+              desc: "The email has been sent successfully."
+            });
+          } else {
+            this.$Notice.error({
+              title: "Email send fail",
+              desc: "Maybe you input a wrong email , please check it out."
+            });
+          }
+        })
+        .catch(err => {
+          console.log(err.data);
+        });
+    },
+    register(){
+      this.$router.push({ name: "Register" });
+    }
+  }
+};
+</script>
+
