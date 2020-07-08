0f01c7391edacc813cd467f80a9e4316d63aaa94
mzy <bryate_mzy@163.com>
2020-07-08 10:24:58 +0800

    Revert "activity 后台"
    This reverts commit 2ebc70162ce92a5d11e65479d16fc79838034c11.

--- GeoProblemSolving-back/src/main/java/cn/edu/njnu/geoproblemsolving/Entity/Activities/PermissionEntity.java
+++
@@ -1,7 +0,0 @@
-package cn.edu.njnu.geoproblemsolving.Entity.Activities;
-
-import lombok.Data;
-
-@Data
-public class PermissionEntity {
-}
--- GeoProblemSolving-back/src/main/java/cn/edu/njnu/geoproblemsolving/Repository/ProtocolRepository.java
+++
@@ -1,9 +0,0 @@
-package cn.edu.njnu.geoproblemsolving.Repository;
-
-import cn.edu.njnu.geoproblemsolving.Entity.Activities.LinkProtocol;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(path = "protocol")
-public interface ProtocolRepository extends MongoRepository<LinkProtocol, String> {
-}
