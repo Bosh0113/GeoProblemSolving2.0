2f71954619a879a447b7b78d6718b16669a73836
zhengzhong <38465619+zhengzhengzhengzheng@users.noreply.github.com>
2021-12-14 22:32:15 +0800

    Temp user

--- GeoProblemSolving-back/src/main/java/cn/edu/njnu/geoproblemsolving/business/activity/service/ShareTokenService.java
+++ GeoProblemSolving-back/src/main/java/cn/edu/njnu/geoproblemsolving/business/activity/service/ShareTokenService.java
@@ -1,14 +1,17 @@
 package cn.edu.njnu.geoproblemsolving.business.activity.service;
 
 import cn.edu.njnu.geoproblemsolving.business.CommonUtil;
+import cn.edu.njnu.geoproblemsolving.business.activity.entity.ShareToken;
+import cn.edu.njnu.geoproblemsolving.business.activity.repository.ShareTokenRepository;
 import cn.edu.njnu.geoproblemsolving.business.user.dao.UserDao;
 import cn.edu.njnu.geoproblemsolving.business.user.entity.UserEntity;
+import com.mongodb.MongoException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.UUID;
+import java.util.*;
 
 /**
  * @ClassName ShareTokenService
@@ -18,54 +21,119 @@ import java.util.UUID;
  **/
 @Service
 public class ShareTokenService {
+    private final Logger LOGGER = LoggerFactory.getLogger(ShareTokenService.class);
+
     @Autowired
     ProjectService projectService;
     @Autowired
     UserDao userDao;
+    @Autowired
+    ShareTokenRepository shareTokenRepository;
 
-    public String setToken(String inviterId, String aid){
+    public HashSet<HashMap<String, String>> getAllToken(String aid, String inviterId){
+        HashSet<HashMap<String, String>> tokenInfos = new HashSet<>();
 
-        // String name = inviterName + ":Temp";
-        //
-        // UserEntity tempUser = new UserEntity();
-        // tempUser.setUserId(uid);
-        // ArrayList<String> joinedProject = new ArrayList<>();
-        // joinedProject.add(aid);
-        // tempUser.setJoinedProjects(joinedProject);
-        // tempUser.setName(name);
-        // userDao.saveLocalUser(tempUser);
+        List<ShareToken> shareTokens = shareTokenRepository.findAllByAid(aid);
+        if (shareTokens == null || shareTokens.size() == 0){
+            //该活动无token，创建一个
+            ShareToken shareToken = new ShareToken();
+            shareToken.setTid(UUID.randomUUID().toString());
+            shareToken.setAid(aid);
+            shareToken.setTempUserName("TempOperator1");
+            shareToken.setCreateTime(new Date());
+            shareToken.setInviterId(inviterId);
+            shareTokenRepository.save(shareToken);
 
-        // //加入项目
-        // projectService.joinProject(aid, uid);
-
-        HashMap<String, String> tempUserInfo = new HashMap<>();
-        String uid = UUID.randomUUID().toString() + inviterId;
-        tempUserInfo.put("userId", uid);
-        tempUserInfo.put("inviterId", inviterId);
-        tempUserInfo.put("aid", aid);
-        return CommonUtil.createToken(tempUserInfo);
-    }
-
-    public UserEntity getTempUserInfo(String jwtToken){
-        HashMap<String, String> tempUserInfo = CommonUtil.getTempUserInfo(jwtToken);
-        if (tempUserInfo == null) return null;
-        String userId = tempUserInfo.get("userId");
-        String aid = tempUserInfo.get("aid");
-
-        UserEntity tempUser = userDao.findUserByIdOrEmail(userId);
-        //当临时被删除时，则将其删除
-        if (tempUser == null){
-            tempUser = new UserEntity();
-            tempUser.setUserId(userId);
-            tempUser.setName("TempOperator" + (int)(Math.random() * 1000) + 1);
-            userDao.saveLocalUser(tempUser);
-
-            //加入项目
-            projectService.joinProject(aid, userId);
+            HashMap<String, String> tokenInfo = new HashMap<>();
+            tokenInfo.put("Token", shareToken.getTid());
+            tokenInfo.put("TempUserName", shareToken.getTempUserName());
+            tokenInfos.add(tokenInfo);
+            return tokenInfos;
         }
-        // return CommonUtil.getTempUserInfo(jwtToken);
-        return tempUser;
+        //有token,则将内容返回
+        for (ShareToken token : shareTokens){
+            HashMap<String, String> tokenInfo = new HashMap<>();
+            tokenInfo.put("Token", token.getTid());
+            tokenInfo.put("TempUserName", token.getTempUserName());
+            tokenInfos.add(tokenInfo);
+        }
+        return tokenInfos;
     }
 
+    public HashMap<String, String> setToken(String inviterId, String aid){
+        ShareToken shareToken = new ShareToken();
+        shareToken.setTid(UUID.randomUUID().toString());
+        shareToken.setAid(aid);
+        shareToken.setCreateTime(new Date());
+        shareToken.setInviterId(inviterId);
+        try {
+            Long tempUserNum = shareTokenRepository.countByAid(aid);
+            shareToken.setTempUserName("TempOperator" + tempUserNum + 1);
+            shareTokenRepository.save(shareToken);
+
+            HashMap<String, String> tokenInfo = new HashMap<>();
+            tokenInfo.put("Token", shareToken.getTid());
+            tokenInfo.put("TempUserName", shareToken.getTempUserName());
+            return tokenInfo;
+        }catch (MongoException e){
+            LOGGER.warn("Failed to create token, mongo throw a exception: " + e);
+            return null;
+        }
+    }
+
+    public ShareToken tokenIsPresent(String tid){
+        Optional<ShareToken> byId = shareTokenRepository.findById(tid);
+        return byId.get();
+    }
+
+    public UserEntity getTempUserInfo(ShareToken shareToken){
+        //有此 token，取出用户，返回给前端
+        String tempUserId = shareToken.getTempUserId();
+        //说明无相关联的用户，说明是第一次使用这token
+        if (tempUserId == null){
+            UserEntity tempUser = new UserEntity();
+            tempUserId = shareToken.getTid() + UUID.randomUUID().toString();
+            String tempUserName = shareToken.getTempUserName();
+            tempUser.setUserId(tempUserId);
+            tempUser.setName(tempUserName);
+            userDao.saveLocalUser(tempUser);
+            //加入项目
+            String aid = shareToken.getAid();
+            projectService.joinProject(aid, tempUserId);
+
+            //更新token
+            shareToken.setTempUserId(tempUserId);
+            shareTokenRepository.save(shareToken);
+
+            return tempUser;
+        }
+        //有此用户将其取出
+        return userDao.findUserByIdOrEmail(tempUserId);
+    }
+
+
+    public boolean delToken(String tid){
+        Optional<ShareToken> byId = shareTokenRepository.findById(tid);
+        if (!byId.isPresent()) {
+            LOGGER.info("No such token, default delete successfully.");
+            return true;
+        }
+        //有此 token
+        ShareToken shareToken = byId.get();
+        //查看有无绑定的用户，有则退出项目并删除临时用户
+        String tempUserId = shareToken.getTempUserId();
+        try {
+            if (tempUserId != null){
+                //退出项目，并删除用户
+                projectService.quitProject(shareToken.getAid(), tempUserId);
+            }
+            //删除token
+            shareTokenRepository.delete(shareToken);
+            return true;
+        }catch (Exception e){
+            LOGGER.warn("Failed to delete token: " + e);
+            return false;
+        }
+    }
 
 }
